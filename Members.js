c874b6e6d50767b5c67064004ccb0cf4c6198e9e
Ashmit <gargashmit09@gmail.com>
2022-10-06 01:00:53 +0530

    Commit 1

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'Ashmit Garg',
+    email: 'gargashmit09@gmail.com',
+    status: 'Active'
+  },
+  {
+    id: 2,
+    name: 'Kumudhini Guptha',
+    email: 'gupthakumudhini@gmail.com',
+    status: 'Inactive'
+  },
+  {
+    id: 3,
+    name: 'Zaheen Khan',
+    email: 'zzzheen@gmail.com',
+    status: 'Active'
+  }
+];
+
+module.exports = members;
